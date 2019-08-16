31f7b9c9b4c4dd82b1e83a3c76d5041d305f60a4
XuanHaPham <phamthixuanha@secure.janeto.com>
2019-08-16 18:43:33 +0700

    init project

---
+++ QuanLyKho/QuanLyKho/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class ControlBarViewModel: BaseViewModel
+    {
+        #region command
+        public ICommand closeWindowCommand { get; set; }
+        #endregion
+        public ControlBarViewModel()
+        {
+            closeWindowCommand = new RelayCommand<UserControl>((p) => { return true; }, (p) => {
+                getUserParent(p);
+                var w = closeWindowCommand as Window;
+                if (w != null)
+                {
+                    w.Close();
+                }
+            });
+            
+        }
+
+        private FrameworkElement getUserParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+            while(parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
---
+++ QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public MainViewModel() {
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+            }
+    }
+}
